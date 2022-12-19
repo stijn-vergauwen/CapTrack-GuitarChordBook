8f079d73f2821b6cc78aadc7217d2962a109d045
Stijn Vergauwen <stijnvergauwen@gmail.com>
2022-12-19 14:27:38 +0100

    make return button and better styling

--- resources/views/chords/info.blade.php
+++ resources/views/chords/info.blade.php
@@ -4,11 +4,25 @@
         Chord
     </x-slot>
 
-    <div class="flex flex-col justify-center items-center min-h-screen">
-        <section class="container max-w-2xl p-12 flex flex-col gap-12 bg-white">
-            <h2 class="font-bold text-2xl">{{ $chord->name }}</h2>
-            
-            <p class="text-lg">{{ $chord->description }}</p>
+    <main class="py-12 flex flex-col items-center gap-12">
+        <section class="container max-w-2xl flex flex-col">
+
+            <div class="w-full mb-6">
+                <a class="py-2 px-4 font-bold text-white bg-blue-500 hover:bg-blue-600 focus:bg-blue-600 transition"
+                    href="{{ route('chordOverview') }}"
+                >
+                    <- Back to overview
+                </a>
+            </div>
+
+            <div class="p-12 bg-white">
+                <p class="font-bold text-lg">Chord name</p>
+                <h2 class="font-bold text-4xl mb-12 text-blue-500">{{ $chord->name }}</h2>
+                
+                <p class="font-bold text-lg">About this chord</p>
+                <p class="text-lg">{{ $chord->description }}</p>
+            </div>
+
         </section>
-    </div>
+    </main>
 </x-layout.base>
