372040a69e95ba3a9db1b4c70736815bbb83cd28
Stijn Vergauwen <stijnvergauwen@gmail.com>
2022-12-19 15:44:27 +0100

    make editor and all functions

---
+++ resources/views/chords/edit.blade.php
@@ -0,0 +1,54 @@
+<x-layout.base>
+
+    <x-slot:title>
+        Edit chord
+    </x-slot>
+
+    <main class="py-12 flex flex-col items-center gap-12">
+        <section class="container max-w-2xl flex flex-col">
+
+            <div class="w-full mb-6">
+                <a class="py-2 px-4 font-bold text-white bg-blue-500 hover:bg-blue-600 focus:bg-blue-600 transition"
+                    href="{{ route('chordInfo', ['id' => $chord->id]) }}"
+                >
+                    <- Back to chord info
+                </a>
+            </div>
+
+            <div class="p-12 flex flex-col gap-8 bg-white">
+                <h2 class="font-bold text-2xl">Edit chord</h2>
+
+                <form class="flex flex-col justify-between gap-8" action="{{ route('chord.edit') }}" method="post">
+                    @csrf
+
+                    <input type="hidden" name="id" value="{{ $chord->id }}">
+
+                    <div>
+                        <label class="block font-semibold text-lg" for="chord-name">Chord name</label>
+                        <input class="border-2 p-2 text-lg w-full"
+                            id="chord-name" name="name" type="text" value="{{ $chord->name }}">
+                    </div>
+
+                    <div>
+                        <label class="block font-semibold text-lg" for="chord-name">Chord description</label>
+                        <input class="border-2 p-2 text-lg w-full"
+                            id="chord-description" name="description" type="text" value="{{ $chord->description }}">
+                    </div>
+
+                    <div class="flex justify-end">
+                        <button class="py-2 px-4 font-bold text-white bg-blue-500 hover:bg-blue-600 focus:bg-blue-600 transition">Save changes</button>
+                    </div>
+                </form>
+
+                <form class="flex justify-end" action="{{ route('chord.delete') }}" method="post">
+                    @csrf
+
+                    <input type="hidden" name="id" value="{{ $chord->id }}">
+
+                    <button class="py-2 px-4 font-bold text-red-400 hover:text-red-600 focus:text-red-600 transition">Delete chord</button>
+                </form>
+            </div>
+
+        </section>
+    </main>
+</x-layout.base>
