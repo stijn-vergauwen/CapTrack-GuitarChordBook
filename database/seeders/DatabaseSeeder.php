0eca80f9aec3e393f525daccd1a10808f5e49f8f
Stijn Vergauwen <stijnvergauwen@gmail.com>
2022-12-21 11:47:02 +0100

    setup finger placement tables

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Chord;
+use App\Models\FingerPlacement;
 use Illuminate\Database\Seeder;
+use App\Models\ChordFingerPlacement;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +17,118 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        Chord::create([
+            'name' => 'C',
+            'description' => 'Beginner C chord',
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
+        FingerPlacement::create([
+            'string' => 1,
+            'fret' => 0,
+            'mute_string' => false,
+        ]);
+
+        FingerPlacement::create([
+            'string' => 2,
+            'fret' => 1,
+            'mute_string' => false,
+        ]);
+
+        FingerPlacement::create([
+            'string' => 3,
+            'fret' => 0,
+            'mute_string' => false,
+        ]);
+
+        FingerPlacement::create([
+            'string' => 4,
+            'fret' => 2,
+            'mute_string' => false,
+        ]);
+
+        FingerPlacement::create([
+            'string' => 5,
+            'fret' => 3,
+            'mute_string' => false,
+        ]);
+
+        FingerPlacement::create([
+            'string' => 6,
+            'fret' => 0,
+            'mute_string' => true,
+        ]);
+
+        ChordFingerPlacement::create([
+            'chord_id' => 1,
+            'finger_placement_id' => 1,
+        ]);
+
+        ChordFingerPlacement::create([
+            'chord_id' => 1,
+            'finger_placement_id' => 2,
+        ]);
+
+        ChordFingerPlacement::create([
+            'chord_id' => 1,
+            'finger_placement_id' => 3,
+        ]);
+
+        ChordFingerPlacement::create([
+            'chord_id' => 1,
+            'finger_placement_id' => 4,
+        ]);
+
+        ChordFingerPlacement::create([
+            'chord_id' => 1,
+            'finger_placement_id' => 5,
+        ]);
+
+        ChordFingerPlacement::create([
+            'chord_id' => 1,
+            'finger_placement_id' => 6,
+        ]);
+
+
+
+
+
+
+
+
+        // dummy data generation, but these chords don't make sense so i don't want to use it atm
+
+        // Chord::factory(5)->create();
+
+        // FingerPlacement::factory(10)->create();
+
+        // ChordFingerPlacement::create([
+        //     'chord_id' => 1,
+        //     'finger_placement_id' => 1,
+        // ]);
+
+        // ChordFingerPlacement::create([
+        //     'chord_id' => 1,
+        //     'finger_placement_id' => 2,
+        // ]);
+
+        // ChordFingerPlacement::create([
+        //     'chord_id' => 1,
+        //     'finger_placement_id' => 3,
+        // ]);
+
+        // ChordFingerPlacement::create([
+        //     'chord_id' => 1,
+        //     'finger_placement_id' => 4,
+        // ]);
+
+        // ChordFingerPlacement::create([
+        //     'chord_id' => 1,
+        //     'finger_placement_id' => 5,
+        // ]);
+
+        // ChordFingerPlacement::create([
+        //     'chord_id' => 1,
+        //     'finger_placement_id' => 6,
         // ]);
     }
 }
