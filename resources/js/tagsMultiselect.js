bdabe1c6ecb72e8734ecf7c3986e6fa139c6d283
Stijn Vergauwen <85249104+stijn301@users.noreply.github.com>
2023-02-23 12:11:14 +0100

    setup js files

---
+++ resources/js/tagsMultiselect.js
@@ -0,0 +1,9 @@
+// These specific files should be imported in html.
+// This holds state & references to correct multiselect element by id, and passes it into the needed functions
+
+// References
+
+
+// State
+
+let selectedTagIds = []
