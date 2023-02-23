681201dc88b40646a77beeb4acbe5adda03ca179
Stijn Vergauwen <85249104+stijn301@users.noreply.github.com>
2023-02-23 10:59:20 +0100

    seed some tags

--- database/seeders/TagSeeder.php
+++ database/seeders/TagSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Tag;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,20 @@ class TagSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Tag::create([
+            'name' => 'Major chord'
+        ]);
+        
+        Tag::create([
+            'name' => 'Minor chord'
+        ]);
+
+        Tag::create([
+            'name' => 'Barre chord'
+        ]);
+
+        Tag::create([
+            'name' => 'Open chord'
+        ]);
     }
 }
